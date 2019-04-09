a38eecbf518fb6883967c41c0eb37261159b7785
God of Monkeys <a8325811@hotmail.com>
2019-04-10 01:15:29 +0800

    修正字庫使用思源黑體
    修正字庫使用思源黑體

--- 字庫區/FontToTGA/chinese.bat
+++ 字庫區/FontToTGA/chinese.bat
@@ -1,7 +1,10 @@
 if not exist Fonts md Fonts
 
-fontToTga.exe -size=24 MicrosoftYaHei Fonts\MicrosoftYaHei24
-fontToTga.exe -bold -size=24 MicrosoftYaHei Fonts\MicrosoftYaHeiBold24
+fontToTga.exe -maxTexSize=2048 -size=20 "Noto Sans CJK TC Regular" Fonts\NotoSansCJK20
+fontToTga.exe -maxTexSize=2048 -size=30 "Noto Sans CJK TC Regular" Fonts\NotoSansCJK30
+
+fontToTga.exe -maxTexSize=2048 -size=20 "Noto Sans CJK TC Bold" Fonts\NotoSansCJK-Bold20
+fontToTga.exe -maxTexSize=2048 -size=30 "Noto Sans CJK TC Bold" Fonts\NotoSansCJK-Bold30
 
 PATH I:\Game\steamapps\common\Arma 3 Tools\TexView2;%PATH%
 for %%a in (Fonts\*.tga) do (
