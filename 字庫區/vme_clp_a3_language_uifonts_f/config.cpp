32995fd5522ca3c19c5d0813150d7f1ddf1154f0
God of Monkeys <a8325811@hotmail.com>
2019-04-15 23:48:46 +0800

    修正字體過小模糊問題
    修正字體過小模糊問題

--- 字庫區/vme_clp_a3_language_uifonts_f/config.cpp
+++ 字庫區/vme_clp_a3_language_uifonts_f/config.cpp
@@ -20,44 +20,44 @@ class CfgFontFamilies
 {
 	class EtelkaMonospacePro
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
 	};
 	class EtelkaMonospaceProBold
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
 	};
 	class EtelkaNarrowMediumPro
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
 		spaceWidth = 0.9;
 		spacing = 0.17;
 	};
 	class PuristaBold
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
 	};
 	class PuristaLight
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
 	};
 	class PuristaMedium
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
 	};
 	class PuristaSemibold
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
 	};
 	class RobotoCondensed
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
 	};
 	class RobotoCondensedBold
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK-Bold30"};
 	};
 	class RobotoCondensedLight
 	{
-		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
+		fonts[] = {"\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK13","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK15","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK20","\vme_clp_a3_language_uifonts_f_data\fonts\NotoSansCJK30"};
 	};
 };
